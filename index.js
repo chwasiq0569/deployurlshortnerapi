10a35ac106bbe0020a493abb0a8552b4e63620aa
wasiq abdullah <chwasiq0569>
2023-01-27 11:28:04 +0500

    added CONCSTR

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config()
 
-mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+mongoose.connect("mongodb+srv://admin:admin@cluster0.bekcudd.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 //use cors to allow cross origin resource sharing
 app.use(
